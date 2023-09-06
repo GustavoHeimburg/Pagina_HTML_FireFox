7333d421bc0ab756a25a49231e09413031b37633
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-06 19:36:51 -0300

    Usando a parte FrontEnd

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+const leia = document.querySelector("h1");
+leia.textContent = "Olá mundo";
+
+let nomePessoa = "Gustavo";
+console.log(nomePessoa);
+nomePessoa = "H.Pereira"
+console.log(nomePessoa);
+
+let tipodeDoce = "chocolate"
+
+if (tipodeDoce == "chocolate"){
+    alert("Sim, é igual a chocolate")
+}else {
+    alert("Nao, Voce esta errado, nao e chocolate")
+}
