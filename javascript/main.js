34959469d9451b2db460075204d4bb15000266e8
Gustavo Heimburg Pereira <gutogustavogu@gmail.com>
2023-09-06 19:52:43 -0300

    Usando a parte FrontEnd

--- javascript/main.js
+++ javascript/main.js
@@ -6,13 +6,25 @@ console.log(nomePessoa);
 nomePessoa = "H.Pereira"
 console.log(nomePessoa);
 
-var tipodeDoce = "chocolate"
+/*var tipodeDoce = "chocolate"
 
 if (tipodeDoce == "chocolate"){
     alert("Sim, é igual a chocolate")
 }else {
     alert("Nao, Voce esta errado, nao e chocolate")
 }
-
 var alerta = document.querySelector("h1")
-alert("Processa um dado")
+alert("Processa um dado")*/
+
+//Multiplic
+
+function multi (valor1, valor2){
+    let result = valor1 * valor2;
+    return result;
+}
+function div (valor1, valor2){
+    let resul = valor1 / valor2
+    return resul;
+}
+console.log(multi(2, 5))
+console.log(div(5, 2))
